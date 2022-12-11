b698a4e1fa134754b6408e89630a2f1e79a3e909
xShankyx <nshanklin22@yahoo.com>
2022-12-11 18:46:28 -0500

    Laptop commit 12/07-12-10 work

---
+++ app/src/main/java/com/example/stocktradingapp/di/RepositoryModule.kt
@@ -0,0 +1,28 @@
+package com.example.stocktradingapp.di
+
+import com.example.stocktradingapp.data.csv.CSVParser
+import com.example.stocktradingapp.data.csv.CompanyListingsParser
+import com.example.stocktradingapp.domain.model.CompanyListing
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCompanyListingsParser(
+        companyListingsParser: CompanyListingsParser
+    ): CSVParser<CompanyListing>
+
+    @Binds
+    @Singleton
+    abstract fun bindStockRepository(
+        stockRepositoryImpl: StockRepositoryImpl
+    ): StockRepository
+
+}
